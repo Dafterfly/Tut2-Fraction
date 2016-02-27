cb4987fdfdc6917c91984c23c367a79f5b709743
Eustacia Rajman <eustacia_rajman@telkomsa.net>
2016-02-27 11:11:06 +0200

    Add constructor

---
+++ Fraction/Fraction.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+class Fraction
+{
+	// will add these later
+}
+
+Fraction::Fraction()
+{
+	int numerator = 0;
+	int denominator = 1; // Can't leave at zero because that is mathemaically nosensical
+}
+
